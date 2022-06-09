1dc8f91006ec20599222b1e0f01309b1d1a25d99
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2022-06-09 17:31:41 +0700

    fix email

--- source/util/mail.js
+++ source/util/mail.js
@@ -1,27 +1,52 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 
-console.log(process.env.EMAIL + '-' + process.env.EMAIL_PW);
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    'https://developers.google.com/oauthplayground'
+  );
 
-const smtpConfig = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PW
-  }
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject('Failed to create access token :(');
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN
+    }
+  });
+
+  return transporter;
 };
 
-const transporter = nodemailer.createTransport(smtpConfig);
-
-transporter.verify((err, success) => {
-  if (err) return new Error(err);
-  console.log('nodemailer config is correct');
-});
+const setUpTransporter = async () => {
+  const transporter = await createTransporter();
+  transporter.use('compile', hbs(handlebarOptions));
+  return transporter;
+};
 
 exports.sendUrlTemplateSoal = async (fileName, filePath, recipient) => {
+  const transporter = await setUpTransporter();
   const address = {
     name: 'Admin Yuk Acak',
     address: process.env.EMAIL
@@ -35,21 +60,8 @@ exports.sendUrlTemplateSoal = async (fileName, filePath, recipient) => {
   });
 };
 
-exports.sendFileHasilAcakan = async (fileName, filePath, recipient) => {
-  const address = {
-    name: 'Admin Yuk Acak',
-    address: process.env.EMAIL
-  };
-  return transporter.sendMail({
-    from: address,
-    to: recipient,
-    subject: 'Hasil Acakan Soal',
-    template: 'sendAttachmentFile',
-    attachments: [{ path: filePath, filename: fileName }]
-  });
-};
-
 exports.sendFiles = async (fileZip, filePdf, recipient) => {
+  const transporter = await setUpTransporter();
   const address = {
     name: 'Admin Yuk Acak',
     address: process.env.EMAIL
@@ -76,5 +88,3 @@ const handlebarOptions = {
   viewPath: path.join(__dirname, '/views/template'),
   extName: '.handlebars'
 };
-
-transporter.use('compile', hbs(handlebarOptions));
